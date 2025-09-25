a2bd9d69494fa49f8442068e7079fe627286335a
IlyaRom82 <light108@tutamail.com>
2025-09-25 22:56:20 +0300

    Перенос репозитория в корень, добавлен README.md, исправлены тесты

---
+++ pages/cart_page.py
@@ -0,0 +1,11 @@
+# PR placeholder: ничего не меняем, просто для отображения изменений2
+from selenium.webdriver.common.by import By
+
+
+class CartPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.checkout_button = (By.ID, "checkout")
+
+    def click_checkout(self):
+        self.driver.find_element(*self.checkout_button).click()
